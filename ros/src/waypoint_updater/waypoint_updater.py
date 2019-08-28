f5ff5306af84b24b898eb90738b6e6f3ee71cfcf
minkvsky <chuanglongwei@163.com>
2019-08-29 00:27:49 +0800

    add get_closest_waypint_idx, loop,

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -31,14 +31,50 @@ class WaypointUpdater(object):
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
 
+
+
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
 
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
+        self.pose = None
+        self.base_waypoints = None
+        self.waypoints_2d = None
+        self.waypoint_tree = None
 
-        rospy.spin()
+        # rospy.spin()
+        self.loop()
+
+    def loop(self):
+        # TODO difference between loop and spin
+        rate = rospy.Rate(50)
+        while not rospy.is_shutdown:
+            if self.pose and self.base_waypoints:
+                closest_waypoint_idx = self.get_closest_waypint_idx()
+                self.publish_waypoints(closest_waypoint_idx)
+            rate.sleep()
+
+
+    def get_closest_waypint_idx(self):
+        x = self.pose.pose.position.x
+        y = self.pose.pose.position.y
+        closest_idx = self.waypoint_tree([x, y], 1)[1]
+
+        closest_coord = self.waypoints_2d(closest_idx)
+        prev_coord = self.waypoints_2d(closest_idx - 1)
+
+        cl_vect = np.array(closest_coord)
+        pre_vect = np.array(prev_coord)
+        pos_vect = np.array([x, y])
+        val = np.dot(cl_vect - pre_vect, pos_vect - cl_vect)
+        if val > 0:
+            closest_idx = (closest_idx + 1) % len(self.waypoints_2d)
+        return closest_idx
+
+    def publish_waypoints(self):
+        pass
 
     def pose_cb(self, msg):
         # TODO: Implement
